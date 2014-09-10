585d4a9315a5c24251332e126bc364403cede181
Christian Danielsen & Patrick David <student@epicodus.com>
2014-09-09 17:17:58 -0700

    add input form for comments WIP

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,25 @@
 class CommentsController < ApplicationController
   def index
     @link = Link.find(params[:link_id])
+    @comment = Comment.new({link_id: params[:link_id]})
     @comments = Comment.where("link_id = ?", params[:link_id])
+    render 'index'
   end
 
   def new
+    @link = Link.find(params[:link_id])
     @comment = Comment.new
+  end
+
+  def create
+    @link = Link.find(params[:link_id])
+    @comment = Comment.new({params[:comment]})
+    foo
+    if @comment.save
+      flash[:notice] = "Comment Created"
+      redirect_to("/links/#{@link.id}/comments")
+    else
+      render('index')
+    end
   end
 end
