f96953af90b48fc0cce830e5d61a7487f6fbb9f9
Jeremy Whitaker <email.jwhitaker@gmail.com>
2014-09-22 11:51:31 -0700

    add capybara coverage for account sign-up

--- spec/features/signin_pages_spec.rb
+++ spec/features/signin_pages_spec.rb
@@ -2,20 +2,36 @@ require 'rails_helper'
 
 describe "the login process" do
   it "signs a user in who uses the right password" do
-    user = User.create(:email => 'user@example.com', :password => 'password')
-    visit '/sessions/new'
+    visit '/users/new'
     fill_in "Email", :with => 'user@example.com'
     fill_in "Password", :with => 'password'
-    click_button "Log In"
-    page.should have_content 'user@example.com'
+    fill_in "Password confirmation", :with => 'password'
+    click_button "Sign Up"
+    page.should have_content 'Account created.'
   end
 
-  it "gives a user an error who uses the wrong password" do
-    visit '/sessions/new'
+  it "alerts the user when the username has been taken" do
+    visit '/users/new'
     user = User.create(:email => 'user@example.com', :password => 'password')
     fill_in 'Email', :with => 'user@example.com'
-    fill_in 'Password', :with => 'wrong'
-    click_button "Log In"
-    page.should have_content 'Sign Up'
+    click_button "Sign Up"
+    page.should have_content 'Email has already been taken'
+  end
+
+  it "alerts the user when the passwords don't match" do
+    visit '/users/new'
+    fill_in "Password", :with => 'password'
+    fill_in "Password confirmation", :with => 'p4ssword'
+    click_button "Sign Up"
+    page.should have_content "Password confirmation doesn't match Password"
+  end
+
+  it "alerts the user when form fields are blank" do
+    visit '/users/new'
+    fill_in "Email", :with => ''
+    fill_in "Password", :with => ''
+    fill_in "Password confirmation", :with => ''
+    click_button "Sign Up"
+    page.should have_content "Password can't be blank"
   end
 end
